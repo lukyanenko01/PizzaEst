c1ccf3ea20fff48619105ff11e3b67ee2b8b5df7
Vlidimir <v.lukyanenko@gmail.com>
2022-12-13 11:14:14 +0200

    Initial Commit

---
+++ KarymPizza/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  KarymPizza
+//
+//  Created by Vladimir Lukyanenko on 13.12.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
